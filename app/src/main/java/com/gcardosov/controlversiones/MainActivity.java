41623e2f873728c9299c4a847d82f274821795b8
gcardoso <ger9047@gmail.com>
2018-05-30 12:32:30 -0500

    Cabios UI y enlazando vista en el java

--- app/src/main/java/com/gcardosov/controlversiones/MainActivity.java
+++ app/src/main/java/com/gcardosov/controlversiones/MainActivity.java
@@ -9,5 +9,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadUI();
+
     }
+
+    private void loadUI(){
+        //logica para cargar la UI
+
+
+    }
+
+
+
+
 }
